5c6a7aa4401e6979132585b3b6ff9e5d388bc15b
Spencer Tuft <spencer.tuft@gmail.com>
2019-12-19 15:49:37 -0700

    Update authorization with logging

--- server/src/auth/index.ts
+++ server/src/auth/index.ts
@@ -1,18 +1,27 @@
-import { Request, Response, NextFunction} from 'express'
-import {DecodedByuJwt} from '../../../byu-jwt-nodejs'
+import {NextFunction, Request, Response} from 'express'
+import {DecodedByuJwt} from 'byu-jwt'
 import {generateMetadataResponseObj, HttpStatus} from '../util/uapi'
 import debug from 'debug'
+
 const logger = debug('email-utility:authorization')
 
-type MiddlewareFunction = (req: Request & { verifiedJWTs?: DecodedByuJwt}, res: Response, next: NextFunction) => any
+type MiddlewareFunction = (req: Request & { verifiedJWTs?: DecodedByuJwt }, res: Response, next: NextFunction) => any
 
-async function authorize(req: Request & { verifiedJWTs?: DecodedByuJwt}, res: Response): Promise<boolean> {
-  return !!req.verifiedJWTs
+async function authorize(req: Request & { verifiedJWTs?: DecodedByuJwt }): Promise<boolean> {
+  const verifiedJWTs = req.verifiedJWTs
+  if (!verifiedJWTs) {
+    logger('Not authorized')
+    return false
+  }
+  logger(`Authorized ${verifiedJWTs.claims.preferredFirstName} ${verifiedJWTs.claims.surname} (${verifiedJWTs.claims.byuId})`)
+  return true
 }
 
-export function middleware (): MiddlewareFunction {
+export function middleware(): MiddlewareFunction {
   return async (req, res, next): Promise<any> => {
-    const authorized = await authorize(req, res)
+    logger('Processing authorization request')
+    const authorized = await authorize(req)
+    logger('Processed authorization request')
     if (!authorized) return res.status(HttpStatus.UNAUTHORIZED).send(generateMetadataResponseObj(HttpStatus.UNAUTHORIZED))
     next()
   }
