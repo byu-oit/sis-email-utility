d3ce34f370d3d8f67dcd0bad312a0e056eb55f1f
Spencer Tuft <spencer.tuft@gmail.com>
2020-01-02 11:47:54 -0700

    Added lodash.get to mailer

--- mailer/src/index.ts
+++ mailer/src/index.ts
@@ -2,22 +2,22 @@
 import uuid from 'uuid'
 import {send} from './ses/messages'
 import {storeMessage} from './s3/messages'
-import {resolveIds} from './validate/messages'
+import {resolveIds} from './external/persons'
 import {Event, Message} from './types/model'
 
 export async function handler (event: Event): Promise<void> {
-  const [to, cc, bcc, from] = await Promise.all([resolveIds(...event.to), resolveIds(...event.cc), resolveIds(...event.bcc), resolveIds(event.from)])
+  const [to, cc, bcc, sender] = await Promise.all([resolveIds(...event.to), resolveIds(...event.cc), resolveIds(...event.bcc), resolveIds(event.from)])
   const message: Message = {
     id: uuid(),
     to,
     cc,
     bcc,
-    from: from[0],
+    from: sender[0],
     subject: event.subject,
     body: event.body,
     status: 'sent',
     status_datetime: new Date().toISOString(),
-    status_by_id: from[0].byuId
+    status_by_id: sender[0].byuId
   }
   await send(message)
   await storeMessage(message)
