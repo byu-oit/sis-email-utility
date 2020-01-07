f7e031a2fb35d727d2172d8dd2f42bec3fe1334f
Spencer Tuft <spencer.tuft@gmail.com>
2020-01-07 10:25:02 -0700

    Change iterator key word

--- mailer/src/validate/index.ts
+++ mailer/src/validate/index.ts
@@ -6,7 +6,7 @@ export function emailNotification (event: SNSEvent): SNSEventMessage {
   const message = JSON.parse(eventStr)
   const expected = ['to', 'cc', 'bcc', 'from', 'subject', 'body',]
   const errors = []
-  for (const prop in expected) {
+  for (const prop of expected) {
     if (!(prop in message)) errors.push(`Missing property "${prop}" in message`)
   }
   if (errors.length) throw new Error(`Validation Errors: ${errors.join(', ')}`)
