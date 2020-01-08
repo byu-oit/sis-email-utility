6e9f61f7b05d8619790311eaaa8f4ebd830ce4e9
Spencer Tuft <spencer.tuft@gmail.com>
2020-01-07 21:56:16 -0700

    Send email to sender if in stg environment

--- server/src/validate/index.ts
+++ server/src/validate/index.ts
@@ -1,8 +1,9 @@
 import {Request} from 'express'
 import {DecodedByuJwt} from 'byu-jwt'
 import {EventMessage, NewMessageBody, Validation} from '../types'
+import {getParams} from '@byu-oit/env-ssm'
 
-export function newMessage(req: Request & { verifiedJWTs: DecodedByuJwt}): Validation<EventMessage> {
+export async function newMessage(req: Request & { verifiedJWTs: DecodedByuJwt }): Promise<Validation<EventMessage>> {
   const errors: string[] = []
 
   const claims = req.verifiedJWTs.claims
@@ -17,9 +18,20 @@ export function newMessage(req: Request & { verifiedJWTs: DecodedByuJwt}): Valid
 
   const {to = [], cc = [], bcc = [], subject = '', body = ''} = req.body as NewMessageBody
   if (!to.length && !cc.length && !bcc.length) errors.push('Requires at least one of: to, cc, bcc.')
-  if (subject === '') errors.push('Required: subject is a non-empty string.')
-  if (body === '') errors.push('Required: body is a non-empty string.')
+  if (subject==='') errors.push('Required: subject is a non-empty string.')
+  if (body==='') errors.push('Required: body is a non-empty string.')
+
+  // Send email only to sender if in stg environment
+  const {HANDEL_ENVIRONMENT_NAME: env} = await getParams(['HANDEL_ENVIRONMENT_NAME'], {prefix: 'HANDEL_PARAMETER_STORE_PREFIX'})
+  if (env === 'stg') {
+    for (const recipients of [to, cc, bcc]) {
+      if (recipients.length){
+        recipients.length = 0
+        recipients.push(sender.netId)
+      }
+    }
+  }
 
   const value = {to, cc, bcc, from: sender, subject, body}
-  return { value, errors }
+  return {value, errors}
 }
