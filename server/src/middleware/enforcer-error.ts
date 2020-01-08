f31b1e31de64ed8f3bcd224478d9fb633aa5c8e5
Spencer Tuft <spencer.tuft@gmail.com>
2020-01-08 08:17:39 -0700

    Use default http status message

--- server/src/middleware/enforcer-error.ts
+++ server/src/middleware/enforcer-error.ts
@@ -4,7 +4,7 @@ import {generateMetadataResponseObj, HttpStatus} from '../util/uapi'
 // TODO - Add better type definitions when openapi enforcer adds them
 export function EnforcerError (err: Error & { exception: any }, req: Request, res: Response, next: NextFunction): any {
   if (err && err.exception && err.exception.header === 'Request has one or more errors') {
-    return res.status(HttpStatus.BAD_REQUEST).send(generateMetadataResponseObj(HttpStatus.BAD_REQUEST, 'Bad Request', err.message.split('\n')))
+    return res.status(HttpStatus.BAD_REQUEST).send(generateMetadataResponseObj(HttpStatus.BAD_REQUEST, undefined, err.message.split('\n')))
   }
   console.error(err.stack)
   return res.status(HttpStatus.INTERNAL_ERROR).send(generateMetadataResponseObj(HttpStatus.INTERNAL_ERROR))
